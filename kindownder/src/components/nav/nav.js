6e2769fd76e7c2be41af3e65e8f196a5d10f991a
Ibrameias <ibramdarwish@gmail.com>
2022-06-24 23:27:57 -0400

    finished aside_nav

--- kindownder/src/components/nav/nav.js
+++ kindownder/src/components/nav/nav.js
@@ -4,11 +4,11 @@ export default function Nav() {
     return (
         <div className='nav__aside'>
             <nav>
-                <img className='logo' src='https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png' alt='logo' />
-                <ul className='aside__nav-links'>
-                    <li><a href='#'><span><ion-icon name="home-outline"></ion-icon></span>Home</a></li>
-                    <li><a href='#'><span><ion-icon name="search-outline"></ion-icon></span>Search</a></li>
-                    <li><a href='#'><span><ion-icon name="library-outline"></ion-icon></span>Your Library</a></li>
+                <ul className='aside__nav-ul'>
+                    <li className='aside__nav-li'><img className='logo' src='https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png' alt='logo' /></li>
+                    <li className='aside__nav-li'><a href='#'><span><ion-icon name="home-outline"></ion-icon></span>Home</a></li>
+                    <li className='aside__nav-li'><a href='#'><span><ion-icon name="search-outline"></ion-icon></span>Search</a></li>
+                    <li className='aside__nav-li'><a href='#'><span><ion-icon name="library-outline"></ion-icon></span>Your Library</a></li>
                 </ul>
             </nav>
         </div>
